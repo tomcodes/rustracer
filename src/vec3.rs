bb672a8c443012e84f2301eea6982673322c425b
tomcodes <tomcodes@users.noreply.github.com>
2021-10-31 20:01:02 +0100

    Implement div ops for Vec3, add tests

--- src/vec3.rs
+++ src/vec3.rs
@@ -41,9 +41,17 @@ impl ops::Mul<f32> for Vec3 {
     }
 }
 
-impl fmt::Display for Vec3 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {})", self.e[0], self.e[1], self.e[2])
+impl ops::Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec3 {
+            e: [
+                self.e[0] / rhs,
+                self.e[1] / rhs,
+                self.e[2] / rhs,
+            ]
+        }
     }
 }
 
@@ -53,7 +61,15 @@ mod tests {
 
     #[test]
     fn test_vec3_add() {
+        assert_eq!(
+            Vec3::new(2.0, 3.0, 5.0) + Vec3::new(3.0, 12.0, 2.0),
+            Vec3::new(5.0, 15.0, 7.0)
+        );
 
+        assert_ne!(
+            Vec3::new(2.0, 3.0, 5.0) + Vec3::new(3.0, 12.0, 2.0),
+            Vec3::new(6.0, 15.0, 7.0)
+        );
     }
 
     #[test]
@@ -63,11 +79,38 @@ mod tests {
 
     #[test]
     fn test_vec3_mul() {
+        assert_eq!(
+            Vec3::new(3.0, 2.0, 0.0) * 15.0,
+            Vec3::new(45.0, 30.0, 0.0)
+        );
 
+        assert_ne!(
+            Vec3::new(3.0, 2.0, 0.0) * 11.0,
+            Vec3::new(45.0, 30.0, 0.0)
+        );
     }
 
     #[test]
     fn test_vec3_div() {
+        assert_eq!(
+            Vec3::new(3.0, 4.0, 6.0) / 2.0,
+            Vec3::new(1.5, 2.0, 3.0)
+        );
+    }
 
+    #[test]
+    fn test_vec3_length() {
+        assert_eq!(
+            Vec3::new(1.0, 2.0, 3.0).length(),
+            (14f32).sqrt()
+        );
+    }
+
+    #[test]
+    fn test_vec3_length_squared() {
+        assert_eq!(
+            Vec3::new(1.0, 2.0, 3.0).length_squared(),
+            14.0
+        );
     }
 }
