a9d65b87323b2be8fbaa0ff62fd0481b1e6b30b6
tomcodes <tomcodes@users.noreply.github.com>
2021-10-31 12:15:03 +0100

    Add basic Ray object

---
+++ src/ray.rs
@@ -0,0 +1,47 @@
+use crate::vec3::Vec3;
+
+pub struct Ray {
+    a: Vec3,
+    b: Vec3
+}
+
+impl Ray {
+    pub fn ray(a: Vec3, b: Vec3) -> Ray {
+        Ray {
+            a: a,
+            b: b
+        }
+    }
+
+    pub fn origin(self) -> Vec3 {
+        self.a
+    }
+
+    pub fn direction(self) -> Vec3 {
+        self.b
+    }
+
+    pub fn point_at_parameter(self, t: f32) -> Vec3 {
+        self.a + self.b * t
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ray_origin() {
+        
+    }
+
+    #[test]
+    fn test_ray_direction() {
+
+    }
+
+    #[test]
+    fn test_ray_point_at_parameter() {
+
+    }
+}
